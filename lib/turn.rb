6e8da32fa4e46acea164c1704471a1d67e703365
Dominic Chapman <dominicchapman@me.com>
2017-09-25 22:46:28 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,6 @@
+
+
+# board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  print " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
+end
