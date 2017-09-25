5f0efbeb7f9f538e29b09e4d052490f2196b3dce
Dominic Chapman <dominicchapman@me.com>
2017-09-25 22:48:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,3 +4,13 @@
 def display_board(board)
   print " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} \n"
 end
+
+def position_taken?(board, index)
+  # position is NOT taken, false
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  # position IS taken, true
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
